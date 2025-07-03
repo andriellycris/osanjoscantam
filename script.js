4bbe8c6b8406348a970a0f1548ce8383d9b3a6e6
andriellycris <andriele.gomes.ferreira@escola.pr.gov.br>
2025-07-03 15:00:31 +0000

    aula3

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
 
-function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
-} 
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  return palavras;
+}
